8d4e4ee1a47975e8a310eaebdbf47ba5336d4406
Leonardo Cech <leonardo.cech@we-bronx.io>
2024-12-02 22:38:46 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/proxy",
+        destination:
+          "https://api.langflow.astra.datastax.com/lf/e1868f5c-c34f-4234-8438-9a355bab9568/api/v1/run/7132e069-bad9-417e-afe1-534680eded56?stream=false",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
